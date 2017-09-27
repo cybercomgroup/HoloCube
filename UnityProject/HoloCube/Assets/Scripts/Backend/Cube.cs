34c0f58bcfdc00f2fef237629b9bdddb9f810080
Robin Edbom <robin.edbom@gmail.com>
2017-09-27 13:22:27 +0200

    Added backend cube classes

--- UnityProject/HoloCube/Assets/Scripts/Backend/Cube.cs
+++ UnityProject/HoloCube/Assets/Scripts/Backend/Cube.cs
@@ -1,7 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Backend;
 using UnityEngine;
 
-public class Cube{
-
+public class Cube
+{
+    public Face Front { get; set; }
+    public Face Back { get; set; }
+    public Face Left { get; set; }
+    public Face Right { get; set; }
+    public Face Top { get; set; }
+    public Face Bottom { get; set; }
 }
