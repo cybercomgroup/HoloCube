98f7c025bbb898527e1655f8ba3d509430367ccf
Robin Edbom <robin.edbom@gmail.com>
2017-10-10 11:43:17 +0200

    Added onClick events for buttons

---
+++ UnityProject/HoloCube/Assets/Scripts/StartSceneOnHoloClick.cs
@@ -0,0 +1,13 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneOnHoloClick : MonoBehaviour,IInputClickHandler
+{
+	public SceneField mySceneAssets;
+
+	public void OnInputClicked(InputClickedEventData eventData)
+	{
+		SceneManager.LoadScene(mySceneAssets.SceneName);
+	}
+}
