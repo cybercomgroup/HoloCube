634c16b7393d32b052acd8a68cff378b582dc1fb
Simon Riemertzon <simon.riemertzon@cybercom.com>
2017-10-16 11:11:35 +0200

    Trying to implement rotationcode - switching branches to pull.

--- CubeLogic/ConsoleApp2/Translator/FaceTranslator.cs
+++ CubeLogic/ConsoleApp2/Translator/FaceTranslator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleApp2
 {
@@ -11,22 +12,17 @@ namespace ConsoleApp2
     {
         private static List<Face> ScannedSides = new List<Face>();
         public Dictionary<CubeSide, Face> faceDict;
-
+        public RubikCube cube = new RubikCube();
 
         private static void makeUpScannedSides()
         {
-            ScannedSides.Add(new Face(CubeColor.White, CubeColor.Green));
-            ScannedSides.Add(new Face(CubeColor.Green, CubeColor.Yellow));
-            ScannedSides.Add(new Face(CubeColor.Yellow, CubeColor.Blue));
-            ScannedSides.Add(new Face(CubeColor.Blue, CubeColor.White));
-            ScannedSides.Add(new Face(CubeColor.Red, CubeColor.Green));
-            ScannedSides.Add(new Face(CubeColor.Orange, CubeColor.Green));
         }
 
         public Translator()
         {
             makeUpScannedSides();
             faceDict = setSides();
+            checkSides(faceDict);
             Console.WriteLine(Print2Dmap());
 
             Console.WriteLine();
@@ -41,9 +37,12 @@ namespace ConsoleApp2
             {
                 s += "\n" + face.Key + "\n";
                 s += "\n";
-               // s += "{0} {1} {2}\n", face.Value.Colors[0].ToString(), face.Value.Colors[1].ToString(), face.Value.Colors[2].ToString();
+                s += "0 1 2 \n";
+
+                //s += "{0} {1} {2}\n", face.Value.Colors[0].ToString(), face.Value.Colors[1].ToString(), face.Value.Colors[2].ToString();
                 s += "7 " + ColorToString(face.Value.MiddleColor) + " 3" + "\n";
                 s += "6 5 4 \n";
+                s += "TopColor: " + ColorToString(face.Value.TopColor) + "\n";
             }
 
             return s;
@@ -81,6 +80,27 @@ namespace ConsoleApp2
             var right = new Face();
             var bot = new Face();
             var left = new Face();
+            //Front
+            ScannedSides.Add(new Face(CubeColor.White, CubeColor.Green));
+            //Top
+            ScannedSides.Add(new Face(CubeColor.Green, CubeColor.Yellow));
+            //Back
+            ScannedSides.Add(new Face(CubeColor.Yellow, CubeColor.Blue));
+            //Bot
+            ScannedSides.Add(new Face(CubeColor.Blue, CubeColor.White));
+            //Right
+            ScannedSides.Add(new Face(CubeColor.Orange, CubeColor.Green));
+            //Left
+            ScannedSides.Add(new Face(CubeColor.Red, CubeColor.Green));
+            
+            cube.Cubies[]
+
+//            ScannedSides.Add(new Face(CubeColor.White, CubeColor.Green));
+//            ScannedSides.Add(new Face(CubeColor.Green, CubeColor.Yellow));
+//            ScannedSides.Add(new Face(CubeColor.Yellow, CubeColor.Blue));
+//            ScannedSides.Add(new Face(CubeColor.Blue, CubeColor.White));
+//            ScannedSides.Add(new Face(CubeColor.Red, CubeColor.Green));
+//            ScannedSides.Add(new Face(CubeColor.Orange, CubeColor.Green));
 
             for (int i = 0; i < ScannedSides.Count; i++)
             {
@@ -94,15 +114,12 @@ namespace ConsoleApp2
                         break;
                     case CubeColor.Yellow:
                         back = GetFaceByColor(CubeColor.Yellow, ScannedSides);
-
-                        break;
-                    case CubeColor.Orange:
-                        right = GetFaceByColor(CubeColor.Orange, ScannedSides);
-
                         break;
                     case CubeColor.Blue:
                         bot = GetFaceByColor(CubeColor.Blue, ScannedSides);
-
+                        break;
+                    case CubeColor.Orange:
+                        right = GetFaceByColor(CubeColor.Orange, ScannedSides);
                         break;
                     case CubeColor.Red:
                         left = GetFaceByColor(CubeColor.Red, ScannedSides);
@@ -111,7 +128,7 @@ namespace ConsoleApp2
                         throw new ArgumentOutOfRangeException();
                 }
             }
-
+         
             var dict = new Dictionary<CubeSide, Face>
             {
                 {CubeSide.Front, front},
@@ -125,6 +142,8 @@ namespace ConsoleApp2
             return dict;
         }
 
+       
+        
 
         public static Face GetFaceByColor(CubeColor color, List<Face> scannedFaces)
         {
