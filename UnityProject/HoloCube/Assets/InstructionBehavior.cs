cd50c2591af4b24d8241c5847bc31cad9ac982f6
Robin Edbom <robin.edbom@gmail.com>
2017-10-16 11:19:23 +0200

    Instructions is now somwhat done...

--- UnityProject/HoloCube/Assets/InstructionBehavior.cs
+++ UnityProject/HoloCube/Assets/InstructionBehavior.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ColorThings;
 using UnityEngine;
 
-public class InstructionBehavior : MonoBehaviour {
+public class InstructionBehavior : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	public TextMesh TextMesh;
+	public MeshRenderer FacingColor;
+	public SpriteRenderer Arrow;
+
+	private int _index;
+	public List<object> ListOfMoves{ get; set; }
+	
+	//for debug;
+	public RubicColors Color;
+	public bool Inverse;
+	public bool DoublMove;
+	
+	private void OnGUI()
+	{
+		if (GUI.Button(new Rect(0,0,100,20), "SetNewInstruction"))
+		{
+			SetNewInstruction(Color,Inverse,DoublMove);
+		}
+	}
+
+	private void Start()
+	{
+		FacingColor.material.shader = Shader.Find("Unlit/Color");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	
+	public void NextMove()
+	{
+		var move = ListOfMoves[_index++];
+//		SetNewInstruction();
+	}
+
+	public void SetNewInstruction(RubicColors facingColor, bool inverse = false, bool doubleMove = false)
+	{
+		FacingColor.material.color = ColorDetection.UnityColorFromEnum(facingColor);
+		Arrow.flipX = inverse;
+		TextMesh.text = doubleMove ? "180°" : "90°";
 	}
 }
